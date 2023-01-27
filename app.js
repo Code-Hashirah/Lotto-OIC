29c5839380319459c46316dd6f44b97dd9705473
Kabir <kabirajibade@yahoo.com>
2023-01-27 17:13:10 +0100

    Dumb lotto codes

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // let number=document.getElementById('number').innerHTML;
-let number='23,45,8'
+let number='13,16,19'
 let  user_guess=[];
 const comp_guess=[]
 let rand;
@@ -9,6 +9,24 @@ for(let i=0; i<=5; i++){
     rand=Math.floor(Math.random()*100)
     console.log(rand)
     comp_guess[i]=rand
+}
+junk();
+
+function junk(){
+    count=0;
+    let l=0;
+    for(values of user_guess){
+        if(values==comp_guess[l]){
+            count++
+        }
+        l++;
+    }
+    if(count==3){
+        console.log("success")
+    }
+    else{
+        console.log("Not it bruv")
+    }
 }
 
 console.log(comp_guess);
