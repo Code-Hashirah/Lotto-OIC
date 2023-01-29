dcb109d20283676c3c67b0e54a5e8122f56f18c0
Kabir <kabirajibade@yahoo.com>
2023-01-29 13:25:36 +0100

    Now styling is all i need to do plus responsiveness

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 // let number=document.getElementById('number').innerHTML;
-let number='71,11,26'
+let number=""
 let  user_guess=[];
 const comp_guess=[]
 let rand;
-// let i=0;
-user_guess=number.split(',')
-for(let i=0; i<=5; i++){
-    rand=Math.floor(Math.random()*100)
-    console.log(rand)
-    comp_guess[i]=rand
+
+function convertToArray(){
+    number=document.getElementById('number').value;
+    // let newNum=number.toString()
+    user_guess=number.split(',')
+    console.log(number)
+    console.log(user_guess)
 }
-junk();
 
 function junk(){
+  convertToArray();
+    for(let i=0; i<=5; i++){
+        rand=Math.floor(Math.random()*100)
+        console.log(rand)
+        comp_guess[i]=rand
+    }
     count=0;
     let l=0;
+   
     for(values of user_guess){
         for(Valus of comp_guess ){
              if(values==Valus){
@@ -26,15 +33,26 @@ function junk(){
     }
     if(count==3 && user_guess.length==3){
         console.log("success! Three direct ")
+        document.getElementById('result').innerHTML="success! Three direct";
     }
     else if(count==2 && user_guess.length==2){
           console.log("success! Two Sure ")
+          document.getElementById('result').innerHTML="success!  Two Sure";
     }
     else{
         console.log("Not it bruv")
+        document.getElementById('result').innerHTML="Not it bruv";
     }
+    document.getElementById('userNum').innerHTML="Your Numbers are "+user_guess;
+document.getElementById('computerNum').innerHTML="The lucky Numbers are "+comp_guess;
 }
 
-console.log(comp_guess);
-console.log(user_guess);
+
+//fnctn
+function Input(a){
+document.getElementById('number').value+=a;
+}
+
+// console.log(comp_guess);
+// console.log(user_guess);
 // console.log(rand)
